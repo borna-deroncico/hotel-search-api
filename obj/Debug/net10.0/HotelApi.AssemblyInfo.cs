1c2402b6838c1f0f2620a39f11c7f8ae063c5a06
borna-deroncico <ljubomir.jeroncic2003@gmail.com>
2026-07-31 00:16:49 +0200

    Upload hotel API

--- obj/Debug/net10.0/HotelApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/HotelApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2489f33da2b3ee47913890acd742d954ef13719")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
